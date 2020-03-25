52172cc5c47306c1a8f20b33ae6395ed4a6c9b9c
cuiwenhao <cuiwenhaozing@163.com>
2020-03-25 10:05:57 +0800

    解决DevToolsActivePort文件不存在报错问题

--- pytest_pomui/conftest.py
+++ pytest_pomui/conftest.py
@@ -4,8 +4,7 @@ from pytest_pomui.pages.login_page import LoginPage
 import time
 from selenium.webdriver.chrome.options import Options
 
-import sys, os
-sys.path.append((os.path.abspath(os.path.join(os.path.dirname(__file__), '../'))))
+
 
 # 命令行参数
 def pytest_addoption(parser):
@@ -26,6 +25,10 @@ def driver(request):
     chrome_options.add_argument('--window-size=1920,1080')  # 设置当前窗口的宽度和高度
     if headless =="yes":
         chrome_options.add_argument('--headless')  # 无界面
+        chrome_options.add_argument('--no-sandbox')  # 解决DevToolsActivePort文件不存在报错问题
+        chrome_options.add_argument('--disable-gpu')  # 禁用GPU硬件加速。如果软件渲染器没有就位，则GPU进程将不会启动。
+        chrome_options.add_argument('--disable-dev-shm-usage')
+
 
     driver = webdriver.Chrome(chrome_options=chrome_options)
 
